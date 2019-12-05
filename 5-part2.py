1566b8a2fb53bc06f10428dce3a419e12dc851a5
Callum Bryson <callumjbryson@gmail.com>
2019-12-05 15:48:22 +1000

    day 5

---
+++ 5-part2.py
@@ -0,0 +1,181 @@
+inputFile = "5-input.txt"
+
+def opAdd(index, arr, A, B, C):
+    input1 = arr[index+1]
+    input2 = arr[index+2]
+
+    loc = arr[index+3]
+
+    if C == 1:
+        param1 = input1
+    else:
+        param1 = arr[input1]
+
+    if B == 1:
+        param2 = input2
+    else:
+        param2 = arr[input2]
+
+    arr[loc] = param1 + param2
+
+def opMult(index, arr, A, B, C):
+    input1 = arr[index+1]
+    input2 = arr[index+2]
+
+    loc = arr[index+3]
+
+    if C == 1:
+        param1 = input1
+    else:
+        param1 = arr[input1]
+
+    if B == 1:
+        param2 = input2
+    else:
+        param2 = arr[input2]
+
+    arr[loc] = param1 * param2
+
+def opSave(index, arr, A, B, C):      
+    input1 = arr[index+1]
+
+    arr[input1] = 5
+
+def opOutput(index, arr, A, B, C):
+    if C == 1:
+        param1 = arr[index+1]
+    else:
+        param1 = arr[arr[index+1]]
+
+    print(param1)
+
+def opJumpIfTrue(index, arr, A, B, C):
+    if C == 1:
+        param1 = arr[index+1]
+    else:
+        param1 = arr[arr[index+1]]
+
+    if B == 1:
+        param2 = arr[index+2]
+    else:
+        param2 = arr[arr[index+2]]
+
+    if param1 != 0:
+        return param2
+    else:
+        return index + 3
+
+def opJumpIfFalse(index, arr, A, B, C):
+    if C == 1:
+        param1 = arr[index+1]
+    else:
+        param1 = arr[arr[index+1]]
+
+    if B == 1:
+        param2 = arr[index+2]
+    else:
+        param2 = arr[arr[index+2]]
+
+    if param1 == 0:
+        return param2
+    else:
+        return index + 3
+
+def opLessThan(index, arr, A, B, C):
+    if C == 1:
+        param1 = arr[index+1]
+    else:
+        param1 = arr[arr[index+1]]
+
+    if B == 1:
+        param2 = arr[index+2]
+    else:
+        param2 = arr[arr[index+2]]
+
+    loc = arr[index+3]
+
+    if param1 < param2:
+        arr[loc] = 1
+    else:
+        arr[loc] = 0
+
+def opEquals(index, arr, A, B, C):
+    if C == 1:
+        param1 = arr[index+1]
+    else:
+        param1 = arr[arr[index+1]]
+
+    if B == 1:
+        param2 = arr[index+2]
+    else:
+        param2 = arr[arr[index+2]]
+
+    loc = arr[index+3]
+
+    if param1 == param2:
+        arr[loc] = 1
+    else:
+        arr[loc] = 0
+
+
+with open(inputFile) as fp:
+    line = fp.readline()
+    arr = line.split(',')
+
+arr = list(map(int, arr))
+
+index = 0
+
+while True:
+    chars = str(arr[index])
+
+    op = 99
+    A = 0
+    B = 0
+    C = 0
+
+    if len(chars) == 5:
+        A = int(chars[0])
+        B = int(chars[1])
+        C = int(chars[2])
+        op = int(chars[3:5])
+    elif len(chars) == 4:
+        B = int(chars[0])
+        C = int(chars[1])
+        op = int(chars[2:4])
+    elif len(chars) == 3:
+        C = int(chars[0])
+        op = int(chars[1:3])
+    else:
+        op = int(chars)
+
+    if op == 1:
+        opAdd(index, arr, A, B, C)
+        index += 4
+    elif op == 2:
+        opMult(index, arr, A, B, C)
+        index += 4
+    elif op == 3:
+        opSave(index,arr, A, B, C)
+        index += 2
+    elif op == 4:
+        opOutput(index,arr, A, B, C)
+        index += 2
+    elif op == 5:
+        index = opJumpIfTrue(index,arr, A, B, C)
+    elif op == 6:
+        index = opJumpIfFalse(index,arr, A, B, C)
+    elif op == 7:
+        opLessThan(index,arr, A, B, C)
+        index += 4
+    elif op == 8:
+        opEquals(index,arr, A, B, C)
+        index += 4
+    elif op == 99:
+        break
+    else:
+        print("error")
+        break
+
+    
+
